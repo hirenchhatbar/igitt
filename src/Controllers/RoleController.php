05ea8b1365e6babe602b6d5129f84af207764e61
Hiren Chhatbar <hc.rajkot@gmail.com>
2023-10-03 12:27:11 +0530

    feat(role): get, post and delete

--- src/Controllers/RoleController.php
+++ src/Controllers/RoleController.php
@@ -5,10 +5,19 @@ use App\Http\Controllers\Controller;
 use Hiren\ApiPlatform\Models\Role;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\View\View;
 
 class RoleController extends Controller
 {
+    /**
+     * The function retrieves a collection of Role objects based on the provided request parameters and
+     * returns it as a JSON response.
+     *
+     * @param Request request The `` parameter is an instance of the `Illuminate\Http\Request`
+     * class. It represents an incoming HTTP request and contains information such as the request
+     * method, headers, query parameters, and request body.
+     *
+     * @return JsonResponse A JsonResponse object is being returned.
+     */
     public function getCollection(Request $request): JsonResponse
     {
         $collection = Role::getCollection($request->query('page_params'), $request->query('filter', []), $request->query('sort_params', []));
@@ -16,18 +25,33 @@ class RoleController extends Controller
         return response()->json($collection);
     }
 
+    /**
+     * The function retrieves a Role object with the specified ID and returns it as a JSON response, or
+     * returns a 404 status code if the Role is not found.
+     *
+     * @param int id The "id" parameter is an integer that represents the unique identifier of a role
+     * in the database.
+     *
+     * @return JsonResponse a JsonResponse.
+     */
     public function get(int $id): JsonResponse
     {
-        $user = ['user' => 'Peter Parker'];
-
-        return response()->json($user);
+        try {
+            return response()->json(Role::findOrFail($id));
+        } catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) {
+            return response()->json(status: 404);
+        }
     }
 
     public function post(Request $request): JsonResponse
     {
-        $user = ['user' => 'Peter Parker'];
-
-        return response()->json($user);
+        try {
+            if ($role = Role::post($request->request->all())) {
+                return response()->json(['id' => $role->id], 201);
+            }
+        } catch (\Illuminate\Database\UniqueConstraintViolationException $e) {
+            return response()->json(status: 409);
+        }
     }
 
     public function put(Request $request, int $id): JsonResponse
@@ -37,10 +61,23 @@ class RoleController extends Controller
         return response()->json($user);
     }
 
+    /**
+     * The function deletes a role with the specified ID and returns an empty JSON response.
+     *
+     * @param int id The `id` parameter is the unique identifier of the role that needs to be deleted.
+     *
+     * @return JsonResponse a JsonResponse.
+     */
     public function delete(int $id): JsonResponse
     {
-        $user = ['user' => 'Peter Parker'];
+        try {
+            $role = Role::findOrFail($id);
 
-        return response()->json($user);
+            $role->delete();
+
+            return response()->json();
+        } catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) {
+            return response()->json(status: 404);
+        }
     }
 }
--- src/Models/Role.php
+++ src/Models/Role.php
@@ -11,6 +11,21 @@ class Role extends Model
      */
     public $timestamps = true;
 
+    /**
+     * The function `getCollection` retrieves a collection of data from a database table based on
+     * specified filters and sorting criteria, and returns the results paginated.
+     *
+     * @param array page The `page` parameter is an array that contains information about pagination.
+     * It has two keys:
+     * @param array filter The `` parameter is an optional array that allows you to filter the
+     * collection based on certain criteria. In this case, it checks if the `name` key is set in the
+     * `` array and if so, it adds a `where` clause to the query to filter by the `
+     * @param array sort The `` parameter is an array that specifies the sorting criteria for the
+     * query. Each key-value pair in the array represents a field to sort by and the direction of the
+     * sorting.
+     *
+     * @return a collection of records from the database that match the given filter and sort criteria.
+     */
     public static function getCollection(array $page, array $filter = [], array $sort = [])
     {
         $query = self::query();
@@ -30,5 +45,28 @@ class Role extends Model
         $query->paginate(perPage: $page['per_page'], page: $page['page']);
 
         return $query->get();
+    }
+
+    /**
+     * The function takes an array of data, creates a new instance of a class, sets its properties
+     * based on the data, saves it to the database, and returns the instance.
+     *
+     * @param array data An array containing the data for creating a new role. The array should have
+     * the following keys:
+     *
+     * @return static an instance of the class that the function is defined in.
+     */
+    public static function post(array $data): static
+    {
+        $role = (new static)->newInstance();
+
+        $role->name = $data['name'];
+        $role->code = \str_replace(' ', '_', \strtoupper($data['name']));
+        $role->ord = $data['ord'];
+        $role->status = $data['status'];
+
+        $role->save();
+
+        return $role;
     }
 }
